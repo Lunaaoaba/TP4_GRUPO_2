1f4c8efbee805b2b0d06c36c2c864079654566b3
Lunaaoaba <lunabaricalla1@gmail.com>
2026-04-27 21:58:44 -0300

    Add DB connection string and query
    Import System.Data.SqlClient and add private fields for database access: a connection string (conexion) pointing to localhost\SQLEXPRESS with Initial Catalog=Viajes, and a consulta string set to "SELECT * FROM Localidades". Prepares the code-behind for future DB operations; Page_Load remains unchanged.

--- TP4_GRUPO_2/Ejercicio1.aspx.cs
+++ TP4_GRUPO_2/Ejercicio1.aspx.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace TP4_GRUPO_2
 {
     public partial class Ejercicio1 : System.Web.UI.Page
     {
+        private const string conexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Viajes;Integrated Security=True;Encrypt=False";
+        private string consulta = "SELECT * FROM Localidades";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
 
         }
     }
