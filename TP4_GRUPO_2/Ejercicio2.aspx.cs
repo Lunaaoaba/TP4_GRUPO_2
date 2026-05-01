5e48d674c1b3faef8858ccea51da6d085d333c52
Lunaaoaba <lunabaricalla1@gmail.com>
2026-05-01 18:34:48 -0300

    Add DB connection fields to Ejercicio2
    Import SqlClient and DynamicData namespaces and add a hardcoded SQL connection string and a default SELECT query to Ejercicio2.aspx.cs. These new fields provide a connection target (local SQLExpress 'Libreria' DB) and a query for retrieving the Productos table, likely preparing the page to access the database. Note: the connection string is currently hardcoded in the source.

--- TP4_GRUPO_2/Ejercicio2.aspx.cs
+++ TP4_GRUPO_2/Ejercicio2.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.DynamicData;
 
 namespace TP4_GRUPO_2
 {
     public partial class Ejercicio2 : System.Web.UI.Page
     {
+        private const string conexionNeptuno = @"Data Source=DESKTOP-Q8FUKTC\\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True;Encrypt=False";
+        private string consultaBdd = "SELECT * FROM Productos";
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
